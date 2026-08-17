81a1e8da772c34139edfeb8c627c7a3c81518fea
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2026-08-17 20:10:14 -0300

    liberando 5174

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
 
   const origens = [
     'http://localhost:5173',
+    'http://localhost:5174',
     'http://127.0.0.1:5173',
     process.env.FRONTEND_URL,
   ].filter((origem): origem is string => Boolean(origem));
