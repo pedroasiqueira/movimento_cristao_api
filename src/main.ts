39f9ddaf9c6a762d15328203871a4e9f35d41c6d
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2026-08-19 16:06:18 -0300

    salvando para deploy

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 void bootstrap();
